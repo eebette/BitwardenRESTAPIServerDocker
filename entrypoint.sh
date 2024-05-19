b36c78a493b3cabec66edf195251f3bfc47d34c8
eebette <eric.bette@pm.me>
2024-05-19 13:01:02 +0900

    Updating entrypoint.sh to handle already logged in error

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,8 @@ else
 	echo "Env variable BW_HOST not detected."
 fi
 
-bw login --apikey
+# Allow login to fail if already logged in
+bw login --apikey || $TRUE
 
 echo 'Running `bw server` on port 8087'
 bw serve --hostname 0.0.0.0
